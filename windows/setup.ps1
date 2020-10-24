411b81be29f6368c573f8379932623e4f519f5db
Daniel Vidali Fryer <daniel-fryer@live.com.au>
2020-10-25 01:05:45 +1100

    Update setup.ps1

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -16,23 +16,42 @@ function Install-FromChocolatey {
 
 Install-Chocolatey
 
+#Install-FromChocolatey 'vscode'
+#Install-FromChocolatey 'wsl2'
+#Install-FromChocolatey 'docker-desktop'
 Install-FromChocolatey 'git'
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/frycast/system-init/master/common/.gitconfig' -OutFile (Join-Path $env:USERPROFILE '.gitconfig')
-Install-FromChocolatey 'git-lfs'
-
-Install-FromChocolatey 'vscode'
+Install-FromChocolatey 'github-desktop'
+#Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/frycast/system-init/master/common/.gitconfig' -OutFile (Join-Path $env:USERPROFILE '.gitconfig')
+#Install-FromChocolatey 'git-lfs'
 Install-FromChocolatey 'microsoft-windows-terminal'
+#Install-FromChocolatey 'powertoys'
 Install-FromChocolatey 'firefox'
 Install-FromChocolatey 'googlechrome'
 Install-FromChocolatey 'vlc'
 Install-FromChocolatey 'r'
 Install-FromChocolatey 'r.studio'
+Install-FromChocolatey 'anaconda3'
+Install-FromChocolatey 'julia'
+#Install-FromChocolatey 'golang'
+#Install-FromChocolatey 'qgis'
+#Install-FromChocolatey 'autohotkey'
 Install-FromChocolatey 'notion'
 Install-FromChocolatey 'sharex'
 Install-FromChocolatey 'everything'
 Install-FromChocolatey 'jabref'
-Install-FromChocolatey 'github-desktop'
-Install-FromChocolatey 'powertoys'
+Install-FromChocolatey 'discord'
+Install-FromChocolatey 'signal'
+Install-FromChocolatey 'slack'
+Install-FromChocolatey 'zoom'
+Install-FromChocolatey 'notion'
+Install-FromChocolatey 'adobereader'
+Install-FromChocolatey 'sumatrapdf'
+Install-FromChocolatey 'wiztree'
+
+
+
+
+
 
 
 
