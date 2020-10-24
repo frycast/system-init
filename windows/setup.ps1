02dccb1e5e5772c426149d576968ac5f85fb568b
Daniel Vidali Fryer <daniel-fryer@live.com.au>
2020-10-25 01:37:32 +1100

    Update setup.ps1

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -1,3 +1,42 @@
+$basics = @(
+    'git', 
+    'github-desktop',
+    'git',
+    'github-desktop',
+    'microsoft-windows-terminal',
+    'firefox',
+    'googlechrome',
+    'vlc',
+    'r',
+    'r.studio',
+    'anaconda3',
+    'julia',
+    'notion',
+    'sharex',
+    'everything',
+    'jabref',
+    'discord',
+    'signal',
+    'slack',
+    'zoom',
+    'notion',
+    'adobereader',
+    'sumatrapdf',
+    'wiztree'
+)
+
+$other = @(
+    'vscode',
+    'wsl2',
+    'docker-desktop',
+ #  'https://raw.githubusercontent.com/frycast/system-init/master/common/.gitconfig' -OutFile (Join-Path $env:USERPROFILE '.gitconfig'),
+    'git-lfs',
+    'powertoys',
+    'golang',
+    'qgis',
+    'autohotkey'
+)
+
 function Install-Chocolatey {
     Set-ExecutionPolicy Bypass -Scope Process -Force;
     [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
@@ -15,39 +54,8 @@ function Install-FromChocolatey {
 }
 
 Install-Chocolatey
-
-#Install-FromChocolatey 'vscode'
-#Install-FromChocolatey 'wsl2'
-#Install-FromChocolatey 'docker-desktop'
-Install-FromChocolatey 'git'
-Install-FromChocolatey 'github-desktop'
-#Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/frycast/system-init/master/common/.gitconfig' -OutFile (Join-Path $env:USERPROFILE '.gitconfig')
-#Install-FromChocolatey 'git-lfs'
-Install-FromChocolatey 'microsoft-windows-terminal'
-#Install-FromChocolatey 'powertoys'
-Install-FromChocolatey 'firefox'
-Install-FromChocolatey 'googlechrome'
-Install-FromChocolatey 'vlc'
-Install-FromChocolatey 'r'
-Install-FromChocolatey 'r.studio'
-Install-FromChocolatey 'anaconda3'
-Install-FromChocolatey 'julia'
-#Install-FromChocolatey 'golang'
-#Install-FromChocolatey 'qgis'
-#Install-FromChocolatey 'autohotkey'
-Install-FromChocolatey 'notion'
-Install-FromChocolatey 'sharex'
-Install-FromChocolatey 'everything'
-Install-FromChocolatey 'jabref'
-Install-FromChocolatey 'discord'
-Install-FromChocolatey 'signal'
-Install-FromChocolatey 'slack'
-Install-FromChocolatey 'zoom'
-Install-FromChocolatey 'notion'
-Install-FromChocolatey 'adobereader'
-Install-FromChocolatey 'sumatrapdf'
-Install-FromChocolatey 'wiztree'
-
+$basics | ForEach-Object { Install-FromChocolatey $_ }
+#$other | ForEach-Object { Install-FromChocolatey $_ }
 
 
 
